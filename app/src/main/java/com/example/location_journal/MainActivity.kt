d004eca00712cb3b2ca4808bd3323d4e89baf4c2
Kenyon Coleman <kenyon.coleman15@gmail.com>
2025-05-06 11:31:01 -0400

    location fixes

--- app/src/main/java/com/example/location_journal/MainActivity.kt
+++ app/src/main/java/com/example/location_journal/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.location_journal
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.location_journal.ui.theme.*
 import com.example.location_journal.viewmodel.JournalViewModel
@@ -21,6 +23,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
 
         setContent {
             LocationJournalTheme {
